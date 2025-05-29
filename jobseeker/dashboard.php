99a03ee6a0bbe66d5d85a62ad44cc368c5fe4067
dsharma00026 <dsharma00026@gmail.com>
2025-05-29 19:11:12 +0530

    fix some bug

--- jobseeker/dashboard.php
+++ jobseeker/dashboard.php
@@ -3,7 +3,6 @@
 <?php
 session_start();
 include("../dbconfig.php");
-include("../includes/header.php");
 ?>
 
 
@@ -23,59 +22,6 @@ include("../includes/header.php");
     </ul>
 </body>
 </html>
-
-
-<?php include("../includes/footer.php"); ?>
-<?php
-session_start();
-include("../dbconfig.php");
-include("../includes/header.php");
-?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-       <link rel="stylesheet" href="../assests/style.css">
-    <title>Job Seeker Dashboard | Punjab Job Portal</title>
-</head>
-<body>
-    <h2>Welcome, <?php echo htmlspecialchars($_SESSION['name']); ?>!</h2>
-    <p>This is your job seeker dashboard.</p>
-    <ul>
-        <li><a href="./viewJob.php">Browse Jobs</a></li>
-        <li><a href="./applyjob.php">My Applications</a></li>
-        <li><a href="../auth/logout.php">Logout</a></li>
-    </ul>
-</body>
-</html>
-
-
-<?php include("../includes/footer.php"); ?>
-<?php
-session_start();
-include("../dbconfig.php");
-include("../includes/header.php");
-?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-       <link rel="stylesheet" href="../assests/style.css">
-    <title>Job Seeker Dashboard | Punjab Job Portal</title>
-</head>
-<body>
-    <h2>Welcome, <?php echo htmlspecialchars($_SESSION['name']); ?>!</h2>
-    <p>This is your job seeker dashboard.</p>
-    <ul>
-        <li><a href="./viewJob.php">Browse Jobs</a></li>
-        <li><a href="./applyjob.php">My Applications</a></li>
-        <li><a href="../auth/logout.php">Logout</a></li>
-    </ul>
-</body>
-</html>
-
 
 <?php include("../includes/footer.php"); ?>
 
